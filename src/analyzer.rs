dbc44205ee6cb393228f545e0ab27f7f5faa1fb4
aThebigbot <thebigboss.garibaldi@gmail.com>
2022-11-06 07:55:42 +0100

    feat(*)!: first commit
    - Added json compiletion\n- Read and colorize output

---
+++ src/analyzer.rs
@@ -0,0 +1,33 @@
+pub fn analyze_string(string: &str) -> Vec<String> {
+    // String format example: name = value;
+
+    let p: Vec<&str> = string.split(" = ").collect();
+    let mut t = "{unknow}";
+
+    if p[1].starts_with("\"") && p[1].ends_with("\"") {
+        t = "string"
+    } else if p[1] == "true" || p[1] == "false" {
+        t = "boolean"
+    } else {
+        t = "number"
+    }
+
+    return vec![p[0].to_string(), p[1].to_string(), t.to_string()];
+}
+
+pub fn convert_to_json(string: &str) -> String {
+    let mut json = String::from("{");
+
+    let lines: Vec<&str> = string.lines().collect();
+
+    for line in lines {
+        let result = analyze_string(line);
+
+        json.push_str(format!("\"{}\": {},", result[0], result[1]).as_str());
+    }
+
+    json.remove(json.len() - 1);
+    json.push_str("}");
+
+    return json;
+}
---
+++ src/main.rs
@@ -0,0 +1,42 @@
+use colored::*;
+use std::env;
+use std::fs;
+
+mod analyzer;
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+
+    if args.len() < 2 {
+        println!(
+            "{}",
+            "# SLIC: data serialization language made just for fun".dimmed()
+        );
+        println!("{} = {}", "version".blue(), "1.0".green());
+        println!("{} = {}", "author".blue(), "\"Thebigbot\"".yellow());
+        println!("{} = {}", "license".blue(), "\"NO-LICENSE\"".yellow());
+        println!("{} = {}", "math".blue(), "1 + 1".green());
+        println!("{} = {}", "you_are_cool".blue(), "true".red());
+        return;
+    }
+
+    let content = fs::read_to_string(&args[1]).expect("Unable to read file");
+
+    if args.len() == 3 {
+        match args[2].as_str() {
+            "json" => {
+                fs::write("./compiled.json", analyzer::convert_to_json(&content)).expect("Unable to write file");
+            }
+            _ => {
+                println!("Options: json or yaml")
+            }
+        }
+    }
+
+    let lines: Vec<&str> = content.lines().collect();
+
+    for line in lines {
+        let result = analyzer::analyze_string(line);
+        println!("{}: {} = {}", result[0].blue(), result[2].yellow(), result[1].green());
+    }
+}
